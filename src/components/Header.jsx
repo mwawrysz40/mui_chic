bf6177aca300d7bf3d893c03bcc702935da2d04a
mwawrysz40 <mwawrysz@wp.pl>
2026-03-18 09:25:53 +0100

    dodanie pól edycyjnych w wyniku próbek

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import logo from '../assets/logo_chic.png'
+//import logo from '../assets/logo_chic.png'
 import { useAuth } from '../auth/AuthProvider'
 import UserMenu from './UserMenu'
 
@@ -16,26 +16,26 @@ export default function Header() {
             <Toolbar>
                 {/* Logo jako osobny element */}
 
-                <Box
-                    component="img"
-                    src={logo}
-                    alt="Logo Chic"
-                    sx={{
-                        height: 40,       // Możesz teraz nieco zwiększyć, bo nie ma białej ramki
-                        width: 'auto',
-                        marginRight: 2,
-                        cursor: 'pointer',
-                        filter: 'drop-shadow(0px 2px 2px rgba(0,0,0,0.3))', // Opcjonalnie: lekki cień samej grafiki
-                        transition: 'transform 0.2s',
-                        '&:hover': {
-                            transform: 'scale(1.1)',
-                        }
-                    }}
-                />
+                {/*<Box*/}
+                {/*    component="img"*/}
+                {/*    src={logo}*/}
+                {/*    alt="Logo Chic"*/}
+                {/*    sx={{*/}
+                {/*        height: 40,       // Możesz teraz nieco zwiększyć, bo nie ma białej ramki*/}
+                {/*        width: 'auto',*/}
+                {/*        marginRight: 2,*/}
+                {/*        cursor: 'pointer',*/}
+                {/*        filter: 'drop-shadow(0px 2px 2px rgba(0,0,0,0.3))', // Opcjonalnie: lekki cień samej grafiki*/}
+                {/*        transition: 'transform 0.2s',*/}
+                {/*        '&:hover': {*/}
+                {/*            transform: 'scale(1.1)',*/}
+                {/*        }*/}
+                {/*    }}*/}
+                {/*/>*/}
 
                 {/* Tytuł jako osobny element obok logo */}
                 <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-                    Chic ERP
+                    ESL Portal
                 </Typography>
 
                 <UserMenu username={user?.username} />
